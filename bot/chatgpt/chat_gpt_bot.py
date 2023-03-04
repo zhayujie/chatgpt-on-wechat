f67dae5b0baaece9294acda8f0802b545a359396
zhayujie <yjzha1996@163.com>
2023-03-04 17:01:26 +0800

    fix: use default max_token

--- bot/chatgpt/chat_gpt_bot.py
+++ bot/chatgpt/chat_gpt_bot.py
@@ -45,7 +45,7 @@ class ChatGPTBot(Bot):
                 model="gpt-3.5-turbo",  # 对话模型的名称
                 messages=query,
                 temperature=0.9,  # 值在[0,1]之间，越大表示回复越具有不确定性
-                max_tokens=4096,  # 回复最大的字符数
+                #max_tokens=4096,  # 回复最大的字符数
                 top_p=1,
                 frequency_penalty=0.0,  # [-2,2]之间，该值越大则更倾向于产生不同的内容
                 presence_penalty=0.0,  # [-2,2]之间，该值越大则更倾向于产生不同的内容
