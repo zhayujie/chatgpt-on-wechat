224ee6bd892633cd822837a21850f8fbccec50e3
zhayujie <yjzha1996@163.com>
2023-03-15 12:57:34 +0800

    fix: openai_base_url load

--- bot/chatgpt/chat_gpt_bot.py
+++ bot/chatgpt/chat_gpt_bot.py
@@ -16,7 +16,8 @@ else:
 class ChatGPTBot(Bot):
     def __init__(self):
         openai.api_key = conf().get('open_ai_api_key')
-        openai.api_base = conf().get('open_ai_api_base')
+        if conf().get('open_ai_api_base'):
+            openai.api_base = conf().get('open_ai_api_base')
         proxy = conf().get('proxy')
         if proxy:
             openai.proxy = proxy
