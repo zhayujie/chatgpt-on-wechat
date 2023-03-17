f5c99aafa4c3540a1fcdd43103c1c70ad6025e5c
yangqijin <514123661@qq.com>
2023-03-18 00:26:26 +0800

    [feat] 新增日志保存文件功能

--- common/log.py
+++ common/log.py
@@ -6,9 +6,16 @@ def _get_logger():
     log = logging.getLogger('log')
     log.setLevel(logging.INFO)
     console_handle = logging.StreamHandler(sys.stdout)
-    console_handle.setFormatter(logging.Formatter('[%(levelname)s][%(asctime)s][%(filename)s:%(lineno)d] - %(message)s',
-                                                  datefmt='%Y-%m-%d %H:%M:%S'))
+    log_formatter = logging.Formatter('[%(levelname)s][%(asctime)s][%(filename)s:%(lineno)d] - %(message)s',
+                                                  datefmt='%Y-%m-%d %H:%M:%S')
+    console_handle.setFormatter(log_formatter)
+
+    file_handle = logging.FileHandler(".log/robot.log",mode="a", encoding="utf_8")
+    file_handle.setLevel(logging.DEBUG)
+    file_handle.setFormatter(log_formatter)
+
     log.addHandler(console_handle)
+    log.addHandler(file_handle)
     return log
 
 
