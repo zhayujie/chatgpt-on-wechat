b3e1f56fb94e5e885d092619ff6ef73207b78617
zhayujie <yjzha1996@163.com>
2023-03-19 01:09:36 +0800

    feat: itchat login hot reload

--- channel/wechat/wechat_channel.py
+++ channel/wechat/wechat_channel.py
@@ -42,7 +42,7 @@ class WechatChannel(Channel):
 
     def startup(self):
         # login by scan QRCode
-        itchat.auto_login(enableCmdQR=2)
+        itchat.auto_login(enableCmdQR=2, hotReload=True)
 
         # start message listener
         itchat.run()
