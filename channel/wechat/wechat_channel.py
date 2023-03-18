d760b045d54e6b8c1f46e40ac37562c6ca22672b
zhayujie <yjzha1996@163.com>
2023-03-19 01:26:53 +0800

    fix: close hot reload because of repeat msg

--- channel/wechat/wechat_channel.py
+++ channel/wechat/wechat_channel.py
@@ -42,7 +42,7 @@ class WechatChannel(Channel):
 
     def startup(self):
         # login by scan QRCode
-        itchat.auto_login(enableCmdQR=2, hotReload=True)
+        itchat.auto_login(enableCmdQR=2)
 
         # start message listener
         itchat.run()
