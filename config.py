5aedce647f1722359799c4177474cbf5863ac014
zhayujie <yjzha1996@163.com>
2023-03-26 02:00:35 +0800

    fix: docker permission bug

--- config.py
+++ config.py
@@ -98,6 +98,8 @@ def load_config():
         config_path = "./config-template.json"
 
     config_str = read_file(config_path)
+    logger.info("[INIT] config str: {}".format(config_str))
+
     # 将json字符串反序列化为dict类型
     config = Config(json.loads(config_str))
 
