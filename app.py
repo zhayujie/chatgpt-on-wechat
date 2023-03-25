eb6fd23a1af94d7df48fba9a148de086816e5d43
514123661 <a514123661@sina.com>
2023-03-25 12:56:49 +0800

    add telegram channel

--- app.py
+++ app.py
@@ -3,15 +3,15 @@
 import config
 from channel import channel_factory
 from common.log import logger
-
+import sys
 from plugins import *
 if __name__ == '__main__':
+    channel_name = 'UnDefined'
     try:
         # load config
         config.load_config()
-
+        channel_name = sys.argv[1]
         # create channel
-        channel_name='wx'
         channel = channel_factory.create_channel(channel_name)
         if channel_name=='wx':
             PluginManager().load_plugins()
@@ -19,5 +19,5 @@ if __name__ == '__main__':
         # startup channel
         channel.startup()
     except Exception as e:
-        logger.error("App startup failed!")
+        logger.error("App startup failed! channel is " + channel_name)
         logger.exception(e)
