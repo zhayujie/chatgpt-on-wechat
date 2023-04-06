06299d4ec3fb160b8cb213567873c55ae33dd6f9
congxu <congxuma@126.com>
2023-04-06 16:04:42 +0800

    Update open_ai_image.py

--- bot/openai/open_ai_image.py
+++ bot/openai/open_ai_image.py
@@ -20,7 +20,7 @@ class OpenAIImage(object):
             response = openai.Image.create(
                 prompt=query,    #图片描述
                 n=1,             #每次生成图片的数量
-                size="256x256"   #图片大小,可选有 256x256, 512x512, 1024x1024
+                size="1024x1024"   #图片大小,可选有 256x256, 512x512, 1024x1024
             )
             image_url = response['data'][0]['url']
             logger.info("[OPEN_AI] image_url={}".format(image_url))
