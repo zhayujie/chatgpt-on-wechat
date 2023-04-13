2dfbc840b362ae80076082725f65edf0943d4316
lanvent <lanvent@qq.com>
2023-04-13 20:44:08 +0800

    chore: save model args as a dict

--- bot/openai/open_ai_bot.py
+++ bot/openai/open_ai_bot.py
@@ -26,6 +26,17 @@ class OpenAIBot(Bot, OpenAIImage):
             openai.proxy = proxy
 
         self.sessions = SessionManager(OpenAISession, model= conf().get("model") or "text-davinci-003")
+        self.args = {
+            "model": conf().get("model") or "text-davinci-003",  # 对话模型的名称
+            "temperature":conf().get('temperature', 0.9),  # 值在[0,1]之间，越大表示回复越具有不确定性
+            "max_tokens":1200,  # 回复最大的字符数
+            "top_p":1,
+            "frequency_penalty":conf().get('frequency_penalty', 0.0),  # [-2,2]之间，该值越大则更倾向于产生不同的内容
+            "presence_penalty":conf().get('presence_penalty', 0.0),  # [-2,2]之间，该值越大则更倾向于产生不同的内容
+            "request_timeout": conf().get('request_timeout', None),  # 请求超时时间，openai接口默认设置为600，对于难问题一般需要较长时间
+            "timeout": conf().get('request_timeout', None), #重试超时时间，在这个时间内，将会自动重试
+            "stop":["\n\n\n"]
+        }
 
     def reply(self, query, context=None):
         # acquire reply content
@@ -64,14 +75,7 @@ class OpenAIBot(Bot, OpenAIImage):
     def reply_text(self, session:OpenAISession, retry_count=0):
         try:
             response = openai.Completion.create(
-                model= conf().get("model") or "text-davinci-003",  # 对话模型的名称
-                prompt=str(session),
-                temperature=0.9,  # 值在[0,1]之间，越大表示回复越具有不确定性
-                max_tokens=1200,  # 回复最大的字符数
-                top_p=1,
-                frequency_penalty=0.0,  # [-2,2]之间，该值越大则更倾向于产生不同的内容
-                presence_penalty=0.0,  # [-2,2]之间，该值越大则更倾向于产生不同的内容
-                stop=["\n\n\n"]
+                prompt=str(session), **self.args
             )
             res_content = response.choices[0]['text'].strip().replace('<|endoftext|>', '')
             total_tokens = response["usage"]["total_tokens"]
