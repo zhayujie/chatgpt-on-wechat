55a6292692b74ceaff485ecd28640bd8bff066fa
格里芬 <115790690+Griffin-tt@users.noreply.github.com>
2023-06-01 17:37:29 +0800

    Update open_ai_image.py

--- bot/openai/open_ai_image.py
+++ bot/openai/open_ai_image.py
@@ -24,7 +24,7 @@ class OpenAIImage(object):
                 api_key=api_key,
                 prompt=query,  # 图片描述
                 n=1,  # 每次生成图片的数量
-                size=conf().get("image_create_size", "256x256"),  # 图片大小,可选有 256x256, 512x512, 1024x1024
+                size=conf().get("image_create_size", "1024x1024"),  # 图片大小,可选有 256x256, 512x512, 1024x1024
             )
             image_url = response["data"][0]["url"]
             logger.info("[OPEN_AI] image_url={}".format(image_url))
