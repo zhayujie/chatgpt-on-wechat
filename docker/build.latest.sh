20dc13b5ac440ac0c0897d45a7cc61a57de89f76
JC0v0 <108552928+JC0v0@users.noreply.github.com>
2023-07-16 11:34:01 +0800

    Update build.latest.sh

--- docker/build.latest.sh
+++ docker/build.latest.sh
@@ -5,4 +5,4 @@ unset KUBECONFIG
 cd .. && docker build -f docker/Dockerfile.latest \
              -t zhayujie/chatgpt-on-wechat .
 
-docker tag zhayujie/chatgpt-on-wechat JC0v0/chatgpt-on-wechat:$(date +%y%m%d)
+docker tag zhayujie/chatgpt-on-wechat zhayujie/chatgpt-on-wechat:$(date +%y%m%d)
