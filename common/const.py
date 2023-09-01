91dc44df530209cbe246ecc3ad7b180f1b1aaed2
resphina <69687075+resphinas@users.noreply.github.com>
2023-09-01 09:38:47 +0800

    Update const.py

--- common/const.py
+++ common/const.py
@@ -5,6 +5,8 @@ BAIDU = "baidu"
 XUNFEI = "xunfei"
 CHATGPTONAZURE = "chatGPTOnAzure"
 LINKAI = "linkai"
+
 VERSION = "1.3.0"
+
 CLAUDEAI = "claude"
 MODEL_LIST = ["gpt-3.5-turbo", "gpt-3.5-turbo-16k", "gpt-4", "wenxin", "xunfei","claude"]
