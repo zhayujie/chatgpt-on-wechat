43e93e8e22050d150ce1861a0fce836e449a4357
Haikui Yang <adehcby@gmail.com>
2024-01-01 22:43:03 +0800

    Update open_ai_image.py

--- bot/openai/open_ai_image.py
+++ bot/openai/open_ai_image.py
@@ -16,13 +16,14 @@ class OpenAIImage(object):
         if conf().get("rate_limit_dalle"):
             self.tb4dalle = TokenBucket(conf().get("rate_limit_dalle", 50))
 
-    def create_img(self, query, retry_count=0, api_key=None):
+    def create_img(self, query, retry_count=0, api_key=None, api_base=None):
         try:
             if conf().get("rate_limit_dalle") and not self.tb4dalle.get_token():
                 return False, "请求太快了，请休息一下再问我吧"
             logger.info("[OPEN_AI] image_query={}".format(query))
             response = openai.Image.create(
                 api_key=api_key,
+                api_base=api_base,
                 prompt=query,  # 图片描述
                 n=1,  # 每次生成图片的数量
                 model=conf().get("text_to_image") or "dall-e-2",
