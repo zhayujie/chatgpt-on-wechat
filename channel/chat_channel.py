73fbb7d5e4eb75d1a33249e113761b66f155f0ff
raJayoux <128459480+JayGarland@users.noreply.github.com>
2024-01-10 18:41:33 -0800

    todo

--- channel/chat_channel.py
+++ channel/chat_channel.py
@@ -280,6 +280,7 @@ class ChatChannel(Channel):
                     reply.content = reply_text
                 elif reply.type == ReplyType.ERROR or reply.type == ReplyType.INFO:
                     reply.content = "[" + str(reply.type) + "]\n" + reply.content
+                #todo replytype pre handle rajayoux
                 elif reply.type == ReplyType.IMAGE_URL or reply.type == ReplyType.VOICE or reply.type == ReplyType.IMAGE or reply.type == ReplyType.FILE or reply.type == ReplyType.VIDEO or reply.type == ReplyType.VIDEO_URL:
                     pass
                 else:
