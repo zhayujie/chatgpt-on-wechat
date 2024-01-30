ced0fa46089b4e4043bacc76a0ec754d20eb25a1
weishao zeng <weishao.zeng@gmail.com>
2024-01-30 10:17:53 +0800

    feat: 增加智谱chatglm4模型支持

--- config.py
+++ config.py
@@ -155,6 +155,10 @@ available_setting = {
     "linkai_api_key": "",
     "linkai_app_code": "",
     "linkai_api_base": "https://api.link-ai.chat",  # linkAI服务地址，若国内无法访问或延迟较高可改为 https://api.link-ai.tech
+
+#   # 智谱AI
+    "zhipu_ai_api_key": "",
+    "zhipu_ai_api_base": "https://open.bigmodel.cn/api/paas/v4",
 }
 
 
