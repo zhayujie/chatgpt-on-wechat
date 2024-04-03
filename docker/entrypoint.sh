68fb6a577a7848adf71ff38617abeb2aeeea5aed
Han Fangyuan <hanfangyuan_ustb@163.com>
2024-04-03 22:17:41 +0800

    feat: update docker image publish workflow (#3)

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,11 +2,11 @@
 set -e
 
 # build prefix
-CHATGPT_ON_WECHAT_PREFIX=${CHATGPT_ON_WECHAT_PREFIX:-""}
+DIFY_ON_WECHAT_PREFIX=${DIFY_ON_WECHAT_PREFIX:-""}
 # path to config.json
-CHATGPT_ON_WECHAT_CONFIG_PATH=${CHATGPT_ON_WECHAT_CONFIG_PATH:-""}
+DIFY_ON_WECHAT_CONFIG_PATH=${DIFY_ON_WECHAT_CONFIG_PATH:-""}
 # execution command line
-CHATGPT_ON_WECHAT_EXEC=${CHATGPT_ON_WECHAT_EXEC:-""}
+DIFY_ON_WECHAT_EXEC=${DIFY_ON_WECHAT_EXEC:-""}
 
 # use environment variables to pass parameters
 # if you have not defined environment variables, set them below
@@ -22,19 +22,19 @@ CHATGPT_ON_WECHAT_EXEC=${CHATGPT_ON_WECHAT_EXEC:-""}
 # export CHARACTER_DESC=${CHARACTER_DESC:-"你是ChatGPT, 一个由OpenAI训练的大型语言模型, 你旨在回答并解决人们的任何问题，并且可以使用多种语言与人交流。"}
 # export EXPIRES_IN_SECONDS=${EXPIRES_IN_SECONDS:-"3600"}
 
-# CHATGPT_ON_WECHAT_PREFIX is empty, use /app
-if [ "$CHATGPT_ON_WECHAT_PREFIX" == "" ] ; then
-    CHATGPT_ON_WECHAT_PREFIX=/app
+# DIFY_ON_WECHAT_PREFIX is empty, use /app
+if [ "$DIFY_ON_WECHAT_PREFIX" == "" ] ; then
+    DIFY_ON_WECHAT_PREFIX=/app
 fi
 
-# CHATGPT_ON_WECHAT_CONFIG_PATH is empty, use '/app/config.json'
-if [ "$CHATGPT_ON_WECHAT_CONFIG_PATH" == "" ] ; then
-    CHATGPT_ON_WECHAT_CONFIG_PATH=$CHATGPT_ON_WECHAT_PREFIX/config.json
+# DIFY_ON_WECHAT_CONFIG_PATH is empty, use '/app/config.json'
+if [ "$DIFY_ON_WECHAT_CONFIG_PATH" == "" ] ; then
+    DIFY_ON_WECHAT_CONFIG_PATH=$DIFY_ON_WECHAT_PREFIX/config.json
 fi
 
-# CHATGPT_ON_WECHAT_EXEC is empty, use ‘python app.py’
-if [ "$CHATGPT_ON_WECHAT_EXEC" == "" ] ; then
-    CHATGPT_ON_WECHAT_EXEC="python app.py"
+# DIFY_ON_WECHAT_EXEC is empty, use ‘python app.py’
+if [ "$DIFY_ON_WECHAT_EXEC" == "" ] ; then
+    DIFY_ON_WECHAT_EXEC="python app.py"
 fi
 
 # modify content in config.json
@@ -44,8 +44,6 @@ fi
 
 
 # go to prefix dir
-cd $CHATGPT_ON_WECHAT_PREFIX
+cd $DIFY_ON_WECHAT_PREFIX
 # excute
-$CHATGPT_ON_WECHAT_EXEC
-
-
+$DIFY_ON_WECHAT_EXEC
