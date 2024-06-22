d011521a88cbe472be47996b0c91e65f686fdd85
bingjiw <391904577@qq.com>
2024-06-22 11:24:46 +0800

    Update ~run.sh

--- ~run.sh
+++ ~run.sh
@@ -1,5 +1,7 @@
+date
 sudo ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 sudo dpkg-reconfigure -f noninteractive tzdata
+date
 
 nohup python3 app.py > output.log 2>&1 &
 tail -f output.log
