07f9801ddf104ecbe45312c6fb7b4d51811a2e6c
Han Fangyuan <hanfangyuan_ustb@163.com>
2024-07-28 18:43:22 +0800

    feat: add gpt-4o and gpt-4o-mini

--- common/const.py
+++ common/const.py
@@ -18,12 +18,14 @@ GPT35 = "gpt-3.5-turbo"
 GPT4 = "gpt-4"
 GPT4_TURBO_PREVIEW = "gpt-4-0125-preview"
 GPT4_VISION_PREVIEW = "gpt-4-vision-preview"
+GPT_4O = "gpt-4o"
+GPT_4O_MINI = "gpt-4o-mini"
 WHISPER_1 = "whisper-1"
 TTS_1 = "tts-1"
 TTS_1_HD = "tts-1-hd"
 
 MODEL_LIST = ["gpt-3.5-turbo", "gpt-3.5-turbo-16k", "gpt-4", "wenxin", "wenxin-4", "xunfei", "claude", "gpt-4-turbo",
-              "gpt-4-turbo-preview", "gpt-4-1106-preview", GPT4_TURBO_PREVIEW, QWEN, GEMINI, DIFY, ZHIPU_AI, COZE]
+              "gpt-4-turbo-preview", "gpt-4-1106-preview", GPT4_TURBO_PREVIEW, GPT_4O, GPT_4O_MINI, QWEN, GEMINI, DIFY, ZHIPU_AI, COZE]
 
 # channel
 FEISHU = "feishu"
