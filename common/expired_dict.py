62c0e2fe5782f131a33d8692bcb6023e62887d6a
Han Fangyuan <hanfangyuan_ustb@163.com>
2024-08-02 22:07:00 +0800

    fix: set default expires time when expires_in_seconds is invalid in ExpiredDict class

--- common/expired_dict.py
+++ common/expired_dict.py
@@ -4,7 +4,7 @@ from datetime import datetime, timedelta
 class ExpiredDict(dict):
     def __init__(self, expires_in_seconds):
         super().__init__()
-        self.expires_in_seconds = expires_in_seconds
+        self.expires_in_seconds = expires_in_seconds if expires_in_seconds else 3600
 
     def __getitem__(self, key):
         value, expiry_time = super().__getitem__(key)
