abf79bf60c1229dbd90006f86f59c6fac9fa5729
vision <vision_wangpc@sina.com>
2024-12-21 17:24:32 +0800

    add gitee-ai model resources that are compatible with openai format

--- bot/chatgpt/chat_gpt_session.py
+++ bot/chatgpt/chat_gpt_session.py
@@ -83,7 +83,7 @@ def num_tokens_from_messages(messages, model):
         tokens_per_message = 3
         tokens_per_name = 1
     else:
-        logger.warn(f"num_tokens_from_messages() is not implemented for model {model}. Returning num tokens assuming gpt-3.5-turbo.")
+        logger.debug(f"num_tokens_from_messages() is not implemented for model {model}. Returning num tokens assuming gpt-3.5-turbo.")
         return num_tokens_from_messages(messages, model="gpt-3.5-turbo")
     num_tokens = 0
     for message in messages:
